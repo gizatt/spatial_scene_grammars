Fix gradient issue in grammar fitting

spatial_scene_grammars/nodes.py
@@ -302,8 +302,7 @@ class Node():
 
     @staticmethod
     def get_variable_ll_given_dicts(value_dict, dist_dict):
-        total_ll = 0.
-        print("Scoring %s in %s" % (value_dict, dist_dict))
+        total_ll = torch.tensor(0.)
         for key, value in value_dict.items():
             total_ll = total_ll + dist_dict[key].log_prob(value).sum()
         return total_ll

spatial_scene_grammars/test/grammar.py
@@ -27,24 +27,31 @@ class Building(HasOnlyXyDerivedVariablesMixin, IndependentSetNode):
     @classmethod
     def get_default_parameters(cls):
         return {
-            "room_spacing": NodeParameter(torch.tensor([5.])),
+            "room_spacing": NodeParameter(torch.tensor([5.]), constraint=constraints.positive),
             "child_probs": NodeParameter(torch.tensor([0.5, 0.5, 0.5, 0.5]), constraint=constraints.unit_interval)
         }
     def get_derived_variable_dists_for_children(self, child_types):
         all_dist_sets = []
         offset = self.room_spacing.get_value()[0]
         for child_type in child_types:
+            # It's important to *not* build the offsets like
+            # tensor([offset[0], 0.0]) -- that strips gradient info.
             child_name = child_type.__name__
             if child_name == "North_Room":
-                dists = {"xy": dist.Delta(self.xy + torch.tensor([0., offset]))}
+                room_offset = torch.zeros(2)
+                room_offset[1] = offset
             elif child_name == "South_Room":
-                dists = {"xy": dist.Delta(self.xy + torch.tensor([0., -offset]))}
+                room_offset = torch.zeros(2)
+                room_offset[1] = -offset
             elif child_name == "East_Room":
-                dists = {"xy": dist.Delta(self.xy + torch.tensor([offset, 0.]))}
+                room_offset = torch.zeros(2)
+                room_offset[0] = offset
             elif child_name == "West_Room":
-                dists = {"xy": dist.Delta(self.xy + torch.tensor([-offset, 0.]))}
+                room_offset = torch.zeros(2)
+                room_offset[0] = -offset
             else:
                 raise ValueError(child_name)
+            dists = {"xy": dist.Normal(self.xy + room_offset, torch.ones(2)*0.001)}
             all_dist_sets.append(dists)
         return all_dist_sets
 

spatial_scene_grammars_examples/planar_box_stacks/grammar.py
@@ -52,10 +52,13 @@ class StackOfN(HasDerivedXy, AndNode):
     def get_derived_variable_dists_for_children(self, child_types):
         all_child_dist_dicts = []
         for k, child in enumerate(child_types):
-            child_xy_dist = dist.Normal(
-                self.xy + torch.tensor([self.x_mean()[0], float(k)]),
-                torch.tensor([self.x_variance()[0], 0.01])
-            )
+            offset = torch.empty(2)
+            offset[0] = self.x_mean()[0]
+            offset[1] = float(k)
+            variance = torch.empty(2)
+            variance[0] = self.x_variance()[0]
+            variance[1] = 0.01
+            child_xy_dist = dist.Normal(self.xy + offset, variance)
             all_child_dist_dicts.append({
                 "xy": child_xy_dist
             })
@@ -77,10 +80,12 @@ class GroupOfN(HasDerivedXy, AndNode):
     def get_derived_variable_dists_for_children(self, child_types):
         all_child_dist_dicts = []
         for k, child in enumerate(child_types):
-            child_xy_dist = dist.Normal(
-                self.xy + torch.tensor([self.x_mean()[0], 0.0]),
-                torch.tensor([self.x_variance()[0], 0.01])
-            )
+            offset = torch.zeros(2)
+            offset[0] = self.x_mean()[0]
+            variance = torch.empty(2)
+            variance[0] = self.x_variance()[0]
+            variance[1] = 0.01
+            child_xy_dist = dist.Normal(self.xy + offset, variance)
             all_child_dist_dicts.append({
                 "xy": child_xy_dist
             })
@@ -109,11 +114,13 @@ class Ground(HasDerivedXy, OrNode):
         for k, child in enumerate(child_types):
             # Spawn the child group at the center of a single box's
             # height, randomly somewhere along the x axis.
-            print("Our x mean: ", id(self.x_mean), self.x_mean, self.x_mean())
-            child_xy_dist = dist.Normal(
-                torch.tensor([self.x_mean()[0], self.xy[1] + 0.5]),
-                torch.tensor([self.x_variance()[0], 0.01])
-            )
+            offset = torch.empty(2)
+            offset[0] = self.x_mean()[0]
+            offset[1] = self.xy[1] + 0.5
+            variance = torch.empty(2)
+            variance[0] = self.x_variance()[0]
+            variance[1] = 0.01
+            child_xy_dist = dist.Normal(self.xy + offset, variance)
             all_child_dist_dicts.append({
                 "xy": child_xy_dist,
             })