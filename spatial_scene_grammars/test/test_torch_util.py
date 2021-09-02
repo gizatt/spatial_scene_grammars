9b13949c47451bd8b39a87616362e32fc3d40261
Greg Izatt <gizatt@mit.edu>
2021-09-01 21:35:56 -0400

    Add translation and rotation interpolation functions

--- spatial_scene_grammars/test/test_torch_util.py
+++ spatial_scene_grammars/test/test_torch_util.py
@@ -5,6 +5,10 @@ import torch
 from spatial_scene_grammars.torch_utils import *
 from torch.distributions import constraints
 
+from pytorch3d.transforms.rotation_conversions import (
+    euler_angles_to_matrix
+)
+
 torch.set_default_tensor_type(torch.DoubleTensor)
 
 @pytest.fixture(params=range(10))
@@ -36,6 +40,52 @@ def test_constrained_param():
     do_tests(torch.tensor([]), constraint=constraints.simplex)
     do_tests(torch.ones(10), constraint=constraints.real)
     do_tests(torch.ones(10)/10., constraint=constraints.simplex)
+
+def test_interpolate_translation():
+    t1 = torch.tensor([0., 0., 0.])
+    t2 = torch.tensor([1., 1., 1])
+
+    t = interp_translation(t1, t2, interp_factor=0.)
+    assert torch.allclose(t, t1), (t, t1)
+
+    t = interp_translation(t1, t2, interp_factor=1.)
+    assert torch.allclose(t, t2), (t, t2)
+
+    t = interp_translation(t1, t2, interp_factor=0.5)
+    expected = torch.tensor([0.5, 0.5, 0.5])
+    assert torch.allclose(t, expected), (t, expected)
+
+    t = interp_translation(t2, t2, interp_factor=0.75)
+    assert torch.allclose(t, t2), (t, t2)
+
+
+
+def test_interpolate_rotation():
+    R1 = euler_angles_to_matrix(torch.tensor([0.0, 0.0, 0.0]), convention="ZYX")
+    R2 = euler_angles_to_matrix(torch.tensor([np.pi/2., 0., 0.]), convention="ZYX")
+
+    R = interp_rotation(R1, R2, interp_factor=0.)
+    assert torch.allclose(R, R1), (R, R1)
+
+    R = interp_rotation(R1, R2, interp_factor=1.)
+    assert torch.allclose(R, R2), (R, R2)
+
+    R = interp_rotation(R2, R2, interp_factor=0.75)
+    assert torch.allclose(R, R2), (R, R2)
+
+    R = interp_rotation(R1, R2, interp_factor=0.5)
+    expected = euler_angles_to_matrix(torch.tensor([np.pi/4., 0., 0.]), convention="ZYX")
+    assert torch.allclose(R, expected), (R, expected)
+
+    R3 = euler_angles_to_matrix(torch.tensor([np.pi, 0., 0.]), convention="ZYX")
+    R = interp_rotation(R1, R3, interp_factor=0.5)
+    angle_distance = torch.arccos((torch.trace(torch.matmul(R.transpose(1, 0), R3)) - 1)/2.)
+    assert torch.isclose(angle_distance, torch.tensor(np.pi/2.)), (angle_distance, np.pi/2.)
+
+    R4 = euler_angles_to_matrix(torch.tensor([3.*np.pi/2., 0., 0.]), convention="ZYX")
+    R = interp_rotation(R1, R4, interp_factor=0.5)
+    expected = euler_angles_to_matrix(torch.tensor([-np.pi/4., 0., 0.]), convention="ZYX")
+    assert torch.allclose(R, expected), (R, expected)
 
 if __name__ == "__main__":
     pytest.main()
--- spatial_scene_grammars/torch_utils.py
+++ spatial_scene_grammars/torch_utils.py
@@ -3,6 +3,11 @@ import torch
 from torch.distributions import constraints, transform_to
 import logging
 
+from pytorch3d.transforms.rotation_conversions import (
+    matrix_to_quaternion, axis_angle_to_matrix,
+    quaternion_to_axis_angle
+)
+
 def inv_softplus(y, beta=1., eps=1E-9):
     # y = 1/beta * log(1 + exp(beta * x))
     # so
@@ -59,3 +64,27 @@ class ConstrainedParameter(torch.nn.Module):
         return self.get_value()
     def get_unconstrained_value(self):
         return self.unconstrained_value
+
+def interp_translation(t1, t2, interp_factor):
+    ''' Linearly interpolates between two translations t1 and t2
+    according to interp_factor. Returns t1 for interp_factor=0
+    and t2 for interp_factor=1. '''
+    assert interp_factor >= 0. and interp_factor <= 1.
+    return t1 * (1. - interp_factor) + t2 * (interp_factor)
+
+def interp_rotation(r1, r2, interp_factor):
+    ''' Given two rotation matrices r1 and r2, returns a rotation
+    that is interp_factor between them; when factor=0, returns r1, and
+    when factor=1, returns r2. Linearly interpolates along the geodesic
+    between the rotations by converting the relative rotation to angle-axis
+    and scaling the angle by interp_factor. If r1 and r2 pi radians apart,
+    the returned rotation axis will be arbitrary. '''
+    assert interp_factor >= 0. and interp_factor <= 1.
+    # Convert to angle-axis, interpolate angle, convert back.
+    # When interp_factor = 0, this return r1. When interp_factor = 1, this
+    # returns 1.
+    rel = torch.matmul(r2, r1.transpose(0, 1))
+    rel_axis_angle = quaternion_to_axis_angle(matrix_to_quaternion(rel))
+    # Scaling keeps axis the same, but changes angle.
+    scaled_rel_axis_angle = interp_factor * rel_axis_angle
+    return torch.matmul(axis_angle_to_matrix(scaled_rel_axis_angle), r1)
