Rerun planar fixed structure vi experiment But with Y variance 2x X variance,to make sure it matches

spatial_scene_grammars_examples/planar_fixed_structure/grammar.py
@@ -79,7 +79,7 @@ class RootNode(AndNode):
                 child_type=HiddenNode,
                 xyz_rule=AxisAlignedGaussianOffsetRule(
                     mean=torch.zeros(3),
-                    variance=torch.tensor([1.0, 1.0, eps])),
+                    variance=torch.tensor([1.0, 2.0, eps])),
                 rotation_rule=SameRotationRule()
             )
         ]