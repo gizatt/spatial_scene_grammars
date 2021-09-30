Ill-fated parsing fix attempt Problem is that I think I shouldn't have the log-probs of inactive rules in the objective. But turning them off (esp the quadratic ones) is tought, so I though I could subtract off the max log prob of the inactive rules, since I can predict what the optimal values will be. But this doesn't work for the rotation case, since sometimes the rotation variables are actually unconstrained and can find other modes of the distribution.

spatial_scene_grammars/parsing.py
@@ -347,7 +347,7 @@ def add_mle_tree_parsing_to_prog(
             else:
                 # Never observed this type in the scene, so this node can't be active.
                 prog.AddLinearConstraint(node.active == 0)
-            
+
     # Finally, build the objective.
     for parent_node in super_tree:
         # For the discrete states, do maximum likelihood.
@@ -371,7 +371,9 @@ def add_mle_tree_parsing_to_prog(
         elif isinstance(parent_node, GeometricSetNode):
             # TODO(gizatt) Is this accurate given max_children causes a truncation
             # of the geometric dist? That might need special handling.
+            # p = p * (1-p)**(n_children)
             p = parent_node.p
+            prog.AddLinearCost(-np.log(p.item()))
             for child in children:
                 prog.AddLinearCost(-np.log(1.-p) * child.active)
         else:
@@ -379,9 +381,20 @@ def add_mle_tree_parsing_to_prog(
 
         ## Child location costs relative to parent.
         for rule, child_node in zip(rules, children):
-            rule.encode_cost(
+            cost_term = rule.encode_cost(
                 prog, rule.xyz_optim_params, rule.rot_optim_params, parent_node, child_node
             )
+            prog.AddCost(cost_term)
+            # Add additional term subtracting off what we know to be the minimum cost
+            # achieved by that optimization, when the child is inactive, so that when
+            # this child is inactive, the functional ll of the node in the optimization is 0.
+            # When the child is inactive, we know all of its children are also inactive, so it'll
+            # not be constrained to match an observed node. So the child pose will be chosen to optimize
+            # the score under this rule relative to the parent.
+            min_possible_cost = -rule.get_max_score().detach().item()
+            print("Rule %s->%s->%s, min cost %s" %(parent_node.name, child.name, rule, min_possible_cost))
+            prog.AddLinearCost((1. - child_node.active) * -min_possible_cost)
+
     return super_tree, observed_nodes, R_random_offset
 
 # Return type of infer_mle_tree_with_mip
@@ -495,6 +508,42 @@ def get_optimized_tree_from_mip_results(inference_results, assert_on_failure=Fal
     if not success:
         logging.warning("MIP structure finding unsuccessful.")
     optimized_tree = SceneTree()
+
+    # Get score per rule in supertree
+    total_active_score = 0.
+    for parent_node in super_tree:
+        children = super_tree.get_children(parent_node)
+        ## Get child rule list.
+        if isinstance(parent_node, GeometricSetNode):
+            rules = [parent_node.rule for k in range(len(children))]
+        elif isinstance(parent_node, (AndNode, OrNode, IndependentSetNode)):
+            rules = parent_node.rules
+        elif isinstance(parent_node, TerminalNode):
+            rules = []
+        else:
+            raise ValueError("Unexpected node type: ", type(parent_node))
+        child_set_score = parent_node.score_child_set([child for child in children if get_sol(child.active) > 0.5]).item()
+        print("%s (active %f): child set score %f" % (parent_node.name, get_sol(parent_node.active), child_set_score))
+        if get_sol(parent_node.active) > 0.5:
+            total_active_score += child_set_score
+        for child, rule in zip(children, rules):
+            proxy_child = deepcopy(child)
+            t_sol = get_sol(child.t_optim)
+            R_sol = RotationMatrix(
+                inference_results.R_random_offset.matrix().dot(
+                    get_sol(child.R_optim_pre_offset))
+            )
+            new_tf = drake_tf_to_torch_tf(RigidTransform(
+                p=t_sol, R=R_sol
+            ))
+            print("child tf: ", new_tf)
+            proxy_child.tf = new_tf
+            child_score = rule.score_child(parent_node, proxy_child, verbose=1).item()
+            print("%s:%s (active %s): child %f" % (parent_node.name, proxy_child.name, get_sol(child.active), child_score))
+            if get_sol(parent_node.active) > 0.5 and get_sol(child.active) > 0.5:
+                total_active_score += child_score
+    print("Total active score: ", total_active_score)
+
     # Build tree top-down so we know parent is already in new tree.
     potential_node_queue = [(super_tree.get_root(), None)]
     while len(potential_node_queue) > 0:
@@ -516,6 +565,7 @@ def get_optimized_tree_from_mip_results(inference_results, assert_on_failure=Fal
             children = list(super_tree.successors(node))
             for child in children:
                 potential_node_queue.append((child, new_node))
+
     return optimized_tree
 
 def get_optimized_trees_from_mip_results(inference_results, assert_on_failure=False):

spatial_scene_grammars/rules.py
@@ -148,12 +148,17 @@ class ProductionRule():
         the xyz and rotation rules matching their parameters (but possibly valued
         by decision variables or fixed values for those parameters), and parent and
         child nodes that have been given R_optim and t_optim decision variable members,
-        encodes the negative log probability of this rule given that parent and child.
+        return the expression for the negative log probability of this rule given
+        that parent and child. (Does *not* add to program, though.) '''
+        return self.xyz_rule.encode_cost(prog, xyz_optim_params, parent, child) + \
+               self.rotation_rule.encode_cost(prog, rot_optim_params, parent, child)
 
-        Returns the return value of encoding the rotation rule (i.e. whether the rotation
-        was fully constrained by the constraints).'''
-        self.xyz_rule.encode_cost(prog, xyz_optim_params, parent, child)
-        self.rotation_rule.encode_cost(prog, rot_optim_params, parent, child)
+    def get_max_score(self):
+        ''' Return the maximum possible score from this rule at its current
+        parameter values for any parent/child pair. (For any rule I can think of, the
+        parent / child poses should not factor in to this number. '''
+        print(self.xyz_rule.get_max_score(), self.rotation_rule.get_max_score())
+        return self.xyz_rule.get_max_score() + self.rotation_rule.get_max_score()
 
 
 ## XYZ Production rules
@@ -206,7 +211,11 @@ class XyzProductionRule():
         t_optim decision variable members, adds the negative log probability
         of this rule given the parent and child to the program.'''
         raise NotImplementedError()
-
+    def get_max_score(self):
+        ''' Return the maximum possible score from this rule at its current
+        parameter values for any parent/child pair. (For any rule I can think of, the
+        parent / child poses should not factor in to this number. '''
+        raise NotImplementedError()
 
 class SamePositionRule(XyzProductionRule):
     ''' Child Xyz is identically parent xyz. '''
@@ -236,7 +245,9 @@ class SamePositionRule(XyzProductionRule):
         for k in range(3):
             prog.AddLinearEqualityConstraint(child.t_optim[k] == parent.t_optim[k])
     def encode_cost(self, prog, optim_params, parent, child):
-        pass
+        return 0.
+    def get_max_score(self):
+        return torch.tensor(0.)
 
 class WorldBBoxRule(XyzProductionRule):
     ''' Child xyz is uniformly chosen in [center, width] in world frame,
@@ -302,7 +313,9 @@ class WorldBBoxRule(XyzProductionRule):
         ub_world = optim_params["center"] + optim_params["width"]/2.
         # log prob = 1 / width on each axis
         total_ll = -sum(ub_world - lb_world)
-        prog.AddLinearCost(-total_ll)
+        return -total_ll
+    def get_max_score(self):
+        return -torch.log(self.width).sum()
 
 
 class AxisAlignedBBoxRule(WorldBBoxRule):
@@ -373,11 +386,13 @@ class AxisAlignedGaussianOffsetRule(XyzProductionRule):
         covar = np.diag(optim_params["variance"])
         inverse_covariance = np.linalg.inv(covar)
         covariance_det = np.linalg.det(covar)
-        log_normalizer = -np.log(np.sqrt( (2. * np.pi) ** 3 * covariance_det))
+        log_normalizer = np.log(np.sqrt( (2. * np.pi) ** 3 * covariance_det))
 
         xyz_offset = child.t_optim - (parent.t_optim + mean)
-        total_ll = -0.5 * (xyz_offset.transpose().dot(inverse_covariance).dot(xyz_offset)) + log_normalizer
-        prog.AddCost(-total_ll)
+        total_ll = -0.5 * (xyz_offset.transpose().dot(inverse_covariance).dot(xyz_offset)) - log_normalizer
+        return -total_ll
+    def get_max_score(self):
+        return self.xyz_dist.log_prob(self.mean).sum()
 
 class WorldFramePlanarGaussianOffsetRule(XyzProductionRule):
     ''' Child xyz is diagonally-Normally distributed relative to parent in world frame
@@ -438,13 +453,15 @@ class WorldFramePlanarGaussianOffsetRule(XyzProductionRule):
         covar = np.diag(optim_params["variance"])
         inverse_covariance = np.linalg.inv(covar)
         covariance_det = np.linalg.det(covar)
-        log_normalizer = -np.log(np.sqrt( (2. * np.pi) ** 2 * covariance_det))
+        log_normalizer = np.log(np.sqrt( (2. * np.pi) ** 2 * covariance_det))
 
         inv_tf = torch_tf_to_drake_tf(self.plane_transform_inv).cast[Expression]()
-        xy_offset = inv_tf.multiply(child.t_optim - parent.t_optim)[:2] - mean
-        total_ll = -0.5 * (xy_offset.transpose().dot(inverse_covariance).dot(xy_offset)) + log_normalizer
-        prog.AddCost(-total_ll)
+        xy_offset = inv_tf.multiply(child.t_optim - parent.t_optim) - mean
+        total_ll = -0.5 * (xy_offset.transpose().dot(inverse_covariance).dot(xy_offset)) - log_normalizer
+        return -total_ll
 
+    def get_max_score(self):
+        return self.xy_dist.log_prob(self.mean).sum()
 
 ## Rotation production rules
 class RotationProductionRule():
@@ -497,7 +514,11 @@ class RotationProductionRule():
         t_optim decision variable members, adds the negative log probability
         of this rule given the parent and child to the program.'''
         raise NotImplementedError()
-
+    def get_max_score(self):
+        ''' Return the maximum possible score from this rule at its current
+        parameter values for any parent/child pair. (For any rule I can think of, the
+        parent / child poses should not factor in to this number. '''
+        raise NotImplementedError()
 
 class SameRotationRule(RotationProductionRule):
     ''' Child Xyz is identically parent xyz. '''
@@ -529,7 +550,9 @@ class SameRotationRule(RotationProductionRule):
         # Child is fully constrained.
         return True
     def encode_cost(self, prog, optim_params, parent, child):
-        pass
+        return 0.
+    def get_max_score(self):
+        return torch.tensor([0.])
 
 class UnconstrainedRotationRule(RotationProductionRule):
     '''
@@ -622,7 +645,10 @@ class UnconstrainedRotationRule(RotationProductionRule):
         # Child rotation not fully constrained.
         return False
     def encode_cost(self, prog, optim_params, parent, child):
-        pass
+        # Constant term
+        return -self.score_child(parent, child).detach().item()
+    def get_max_score(self):
+        return torch.log(torch.ones(1) / np.pi**2)
 
 def recover_relative_angle_axis(parent, child, target_axis, zero_angle_width=1E-2, allowed_axis_diff=10. * np.pi/180.):
     # Recover angle-axis relationship between a parent and child.
@@ -806,7 +832,10 @@ class UniformBoundedRevoluteJointRule(RotationProductionRule):
         return False
 
     def encode_cost(self, prog, optim_params, parent, child):
-        pass
+        # Uniform distribution over angles, constant term
+        return torch.log(self.width).sum().detach().numpy()
+    def get_max_score(self):
+        return -torch.log(self.width).sum()
 
 class GaussianChordOffsetRule(RotationProductionRule):
     ''' Placeholder '''
@@ -927,7 +956,10 @@ class GaussianChordOffsetRule(RotationProductionRule):
         total_ll = dot_product * concentration - vmf_normalizer
         # Instead, this happens to be?
         total_ll = vector_diff.sum() * concentration - vmf_normalizer
-        prog.AddCost(-total_ll)
+        return -total_ll
+
+    def get_max_score(self):
+        return self._angle_dist.log_prob(self.loc)
 
 wfbrr_warned_prior_detached = False
 wfbrr_warned_params_detached = False
@@ -1126,8 +1158,10 @@ class WorldFrameBinghamRotationRule(RotationProductionRule):
             R[2, 2] == 1 - 2*xx - 2*yy
         )
 
-        # Add cost based on quaternion terms.
-        # Negate since we're maximizing log prob (i.e.
-        # minimizing -ll.)
-        cost = -np.trace(Z.dot(M.T.dot(qqt.dot(M))))
-        prog.AddLinearCost(cost)
+        # Add log prob based on quaternion terms.
+        ll = np.trace(Z.dot(M.T.dot(qqt.dot(M)))) - np.log(self._bingham_dist._norm_const.item())
+
+        return -ll
+
+    def get_max_score(self):
+        return self._bingham_dist.log_prob(self.M[:, -1])

spatial_scene_grammars_examples/dish_bin/grammar.py
@@ -237,7 +237,7 @@ class Object(OrNode):
         ]
         return ObjectRules
 
-class AssortedObjects(GeometricSetNode):
+class AssortedPlates(GeometricSetNode):
     def __init__(self, tf):
         super().__init__(
             tf=tf,
@@ -249,14 +249,52 @@ class AssortedObjects(GeometricSetNode):
     @classmethod
     def generate_rules(cls):
         rule = ProductionRule(
-            child_type=Object,
+            child_type=Plate,
             xyz_rule=AxisAlignedGaussianOffsetRule(
                 mean=torch.tensor([0.0, 0.0, 0.025]),
                 variance=torch.tensor([0.05, 0.05, 0.05])),
             rotation_rule=WorldFrameBinghamRotationRule(torch.eye(4), torch.tensor([-1, -1, -1, 0.]))
         )
         return [rule]
-
+class AssortedCups(GeometricSetNode):
+    def __init__(self, tf):
+        super().__init__(
+            tf=tf,
+            p=0.2,
+            max_children=6,
+            physics_geometry_info=None,
+            observed=False
+        )
+    @classmethod
+    def generate_rules(cls):
+        rule = ProductionRule(
+            child_type=Cup,
+            xyz_rule=AxisAlignedGaussianOffsetRule(
+                mean=torch.tensor([0.0, 0.0, 0.025]),
+                variance=torch.tensor([0.05, 0.05, 0.05])),
+            rotation_rule=WorldFrameBinghamRotationRule(torch.eye(4), torch.tensor([-1, -1, -1, 0.]))
+        )
+        return [rule]
+class AssortedBowls(GeometricSetNode):
+    def __init__(self, tf):
+        super().__init__(
+            tf=tf,
+            p=0.2,
+            max_children=6,
+            physics_geometry_info=None,
+            observed=False
+        )
+    @classmethod
+    def generate_rules(cls):
+        rule = ProductionRule(
+            child_type=Bowl,
+            xyz_rule=AxisAlignedGaussianOffsetRule(
+                mean=torch.tensor([0.0, 0.0, 0.025]),
+                variance=torch.tensor([0.05, 0.05, 0.05])),
+            rotation_rule=WorldFrameBinghamRotationRule(torch.eye(4), torch.tensor([-1, -1, -1, 0.]))
+        )
+        return [rule]
+    
 class AssortedPlateStacks(GeometricSetNode):
     def __init__(self, tf):
         super().__init__(
@@ -306,7 +344,7 @@ class DishBin(IndependentSetNode):
         geom.register_model_file(torch.eye(4), "sink/bin.sdf")
         super().__init__(
             tf=tf,
-            rule_probs=torch.tensor([0.5, 0.5, 0.5]),
+            rule_probs=torch.tensor([0.5, 0.5, 0.5, 0.5, 0.5]),
             physics_geometry_info=geom,
             observed=True
         )
@@ -314,7 +352,17 @@ class DishBin(IndependentSetNode):
     def generate_rules(cls):
         return [
             ProductionRule(
-                child_type=AssortedObjects,
+                child_type=AssortedPlates,
+                xyz_rule=SamePositionRule(),
+                rotation_rule=SameRotationRule()
+            ),
+            ProductionRule(
+                child_type=AssortedCups,
+                xyz_rule=SamePositionRule(),
+                rotation_rule=SameRotationRule()
+            ),
+            ProductionRule(
+                child_type=AssortedBowls,
                 xyz_rule=SamePositionRule(),
                 rotation_rule=SameRotationRule()
             ),